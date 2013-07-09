b3a1cf307c0a4358b27b3c04e25a261c6fac0d04
Eckhard Ortwein <eckhard.ortwein@addressm.net>
2013-07-09 23:58:11 +0000

    updated web.js from file1

--- web.js
+++ web.js
@@ -1,18 +1,17 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
-var fs =require('fs');
-buf = new Buffer(128);
+
 
 
 app.get('/', function(request, response) {
-  Buffer.toString(fs.readFileSync('index.html'));
---'Hello World2!');
-});
+var fs = require('fs');
+var buffer = new buffer ();
+  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")))});
+ });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
---  fs.readFileSync('index.html');
 
 });
