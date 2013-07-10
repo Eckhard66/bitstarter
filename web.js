b12f5c7e2bdd4b542d1242ec727adb158682fcf2
Eckhard Ortwein <eckhard.ortwein@addressm.net>
2013-07-10 09:59:16 +0000

    updated web.js from file1

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 var express = require('express');
-var app = express.createServer(express.logger());
-
-
+//var app = express.createServer(express.logger());
+//var express = require("express");
+  var app = express()
 
 app.get('/', function(request, response) {
-var fs = require('fs');
-var buffer = new buffer ();
-  response.send(buffer.toString('utc 8', fs.readFileSync("index.html")))});
+  var fs = require('fs');
+  var buf = new Buffer (fs.readFileSync('index.html', 'utf-8'));
+  response.send(buf.toString() ) ;
  });
 
 
